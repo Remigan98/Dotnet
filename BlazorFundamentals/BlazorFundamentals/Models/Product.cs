1eb61a840a21ea9a0e3aa60b69133dfa0f541352
Remigiusz Ganiec <remekgc@gmail.com>
2025-12-03 21:00:45 +0100

    Initialize BlazorFundamentals project structure
    Add initial BlazorFundamentals solution and project files, including core Razor components, layouts, pages, models, configuration, and static assets. Includes Bootstrap for styling and sets up routing, error handling, and basic interactive features.

---
+++ BlazorFundamentals/BlazorFundamentals/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorFundamentals.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<ProductProp> Properties { get; set; } = Enumerable.Empty<ProductProp>();
+    }
+}
