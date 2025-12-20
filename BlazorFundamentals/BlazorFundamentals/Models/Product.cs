d91618c6dd2664ee12306162b3216bf38e053902
Remigiusz Ganiec <remekgc@gmail.com>
2025-12-20 21:59:07 +0100

    Add Blazor Forms page and enhance product selection UI
    Introduced a new Blazor Forms page and updated the navigation menu to include it. Enhanced the product selection experience in DemoProductActions by highlighting the selected product using a new CSS class and logic in _ProductView. Extended the Product model with validation attributes and new properties, including Category and AvailableAfter.

--- BlazorFundamentals/BlazorFundamentals/Models/Product.cs
+++ BlazorFundamentals/BlazorFundamentals/Models/Product.cs
@@ -1,11 +1,25 @@
-﻿namespace BlazorFundamentals.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFundamentals.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public double Price { get; set; }
+        public required string Name { get; set; }
+        [Range(1,1000)] public required double Price { get; set; }
         public bool IsActive { get; set; }
         public IEnumerable<ProductProp> Properties { get; set; } = Enumerable.Empty<ProductProp>();
+
+        public Category Category { get; set; }
+        public DateOnly AvailableAfter { get; set; }
+    }
+
+    public enum Category
+    {
+        Electronics,
+        Clothing,
+        HomeGoods,
+        Books,
+        Toys
     }
 }
