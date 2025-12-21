ff6751632e7ab2c2539af7ee6464cb5373e219ff
Remigiusz Ganiec <remekgc@gmail.com>
2025-12-21 15:30:21 +0100

    Add product creation form and update Product model
    Introduced a Blazor form for creating products with validation and feedback in BlazorForms.razor. Updated the Product model to use default values and explicit [Required] attributes for better form integration.

--- BlazorFundamentals/BlazorFundamentals/Models/Product.cs
+++ BlazorFundamentals/BlazorFundamentals/Models/Product.cs
@@ -5,13 +5,13 @@ namespace BlazorFundamentals.Models
     public class Product
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
-        [Range(1,1000)] public required double Price { get; set; }
+        [Required] public string Name { get; set; } = string.Empty;
+        [Range(1, 1000)] public double Price { get; set; } = 1;
         public bool IsActive { get; set; }
         public IEnumerable<ProductProp> Properties { get; set; } = Enumerable.Empty<ProductProp>();
 
         public Category Category { get; set; }
-        public DateOnly AvailableAfter { get; set; }
+        public DateOnly AvailableAfter { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     }
 
     public enum Category
