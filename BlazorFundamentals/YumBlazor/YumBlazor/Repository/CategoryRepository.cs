85018ffcf30b10a696f83e1a0b202d966b7b996c
Remigiusz Ganiec <remekgc@gmail.com>
2025-12-21 19:21:54 +0100

    Add initial Blazor Identity and account components
    Introduces Blazor Identity UI components, account management pages, authentication logic, and supporting infrastructure for user registration, login, password reset, and external login. Also adds supporting data models, repository interfaces, migrations, and static assets for the YumBlazor application.

---
+++ BlazorFundamentals/YumBlazor/YumBlazor/Repository/CategoryRepository.cs
@@ -0,0 +1,70 @@
+﻿using YumBlazor.Data;
+using YumBlazor.Repository.Interfaces;
+
+namespace YumBlazor.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        readonly ApplicationDbContext dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Category Create(Category category)
+        {
+            dbContext.Categories.Add(category);
+            dbContext.SaveChanges();
+
+            return category;
+        }
+
+        public bool Delete(int id)
+        {
+            Category? obj = dbContext.Categories.Find(id);
+
+            if (obj is null)
+            {
+                return false;
+            }
+
+            dbContext.Categories.Remove(obj);
+
+            return dbContext.SaveChanges() > 0;
+        }
+
+        public Category Get(int id)
+        {
+            Category? category = dbContext.Categories.Find(id);
+
+            if (category is null)
+            {
+                return new Category();
+            }
+
+            return category;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return dbContext.Categories.ToList();
+        }
+
+        public Category Update(Category category)
+        {
+            Category? existingCategory = dbContext.Categories.FirstOrDefault(c => c.Id == category.Id);
+
+            if (existingCategory is not null)
+            {
+                existingCategory.Name = category.Name;
+                dbContext.Categories.Update(existingCategory);
+                dbContext.SaveChanges();
+
+                return existingCategory;
+            }
+
+            return category;
+        }
+    }
+}
---
+++ BlazorFundamentals/YumBlazor/YumBlazor/Repository/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        public Category Create(Category category);
+        public Category Update(Category category);
+        public bool Delete(int id);
+        public Category Get(int id);
+        public IEnumerable<Category> GetAll();
+    }
+}
