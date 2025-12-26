2b5c50754b52406de6ee98ddcd5452e02ac7ed1a
Remigiusz Ganiec <remekgc@gmail.com>
2025-12-26 18:29:10 +0100

    Add role-based registration and new navigation bar
    Refactored login and registration pages with improved UI and added role selection during registration. Introduced StaticDetails for role constants and enabled role management in Identity. Added a new NavBar component for authenticated users and updated MainLayout to conditionally display navigation based on authentication and role. Included product images in wwwroot.

---
+++ BlazorFundamentals/YumBlazor/YumBlazor/Utility/StaticDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace YumBlazor.Utility
+{
+    public static class StaticDetails
+    {
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+    }
+}
