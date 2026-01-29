3c9aa3d826fc3fb855e3f6f1d29a80da3f754f1f
Remigiusz Ganiec <remekgc@gmail.com>
2026-01-29 23:11:19 +0100

    Add ApplicationDbContextFactory and update migration workflow
    Introduced ApplicationDbContextFactory to support design-time DbContext creation using a connection string from environment variables. Updated Azure deployment workflow to set the CONNECTION_STRING environment variable for database migrations.

---
+++ BlazorFundamentals/YumBlazor/YumBlazor/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace YumBlazor.Data;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        // Use connection string from command line args or environment variable
+        var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+            ?? "Server=(localdb)\\mssqllocaldb;Database=YumBlazor;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
